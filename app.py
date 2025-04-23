a62facd990265ec9f0fa58b176945986ae15bb15
MrTradeBuddy <yolkgames@gmail.com>
2025-04-23 20:07:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,10 +26,9 @@ def home():
                 border-radius: 10px;
                 box-shadow: 0 0 10px rgba(0,0,0,0.1);
                 text-align: center;
-                position: absolute;
-                top: 20%;
-                left: 50%;
-                transform: translate(-50%, -20%);
+                width: 100%;
+                max-width: 400px;
+                margin: 100px auto 0 auto;
             }
             input, button {
                 padding: 10px;
