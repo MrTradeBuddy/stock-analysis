775c8d73044797e4589b495d3ff73ab8d8ab23af
MrTradeBuddy <yolkgames@gmail.com>
2025-04-27 14:55:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles  # ⭐ இது மிக முக்கியம்
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")  # ⭐ static serve
+# ⭐ MOUNT STATIC FILES
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# ⭐ Jinja2 templates set
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
