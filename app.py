c2ec59f1077d127048d52da509a1a4e039e1acb0
MrTradeBuddy <yolkgames@gmail.com>
2025-04-27 14:50:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,13 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.routing import APIRouter
-from starlette.responses import RedirectResponse
-import uvicorn
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-# Mount static files
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-# Templates
 templates = Jinja2Templates(directory="templates")
 
-# Routes
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
-
-if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=10000, reload=True)
