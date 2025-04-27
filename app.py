379601c00d872188a85f63955ab482d93e726b23
MrTradeBuddy <yolkgames@gmail.com>
2025-04-27 14:08:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
