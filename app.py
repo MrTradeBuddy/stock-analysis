1888de7191e740756d2e03b5528a2dc45275dd79
MrTradeBuddy <yolkgames@gmail.com>
2025-04-28 17:05:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,3 +13,4 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
