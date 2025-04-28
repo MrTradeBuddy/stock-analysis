b9cc288fc6d5454b8a2926f9ba6a0815246316b0
MrTradeBuddy <yolkgames@gmail.com>
2025-04-28 15:38:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+# Static files serve பண்ண (css/js/image க்கு)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Template folder pointing
+templates = Jinja2Templates(directory="templates")
+
+# Home page
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
