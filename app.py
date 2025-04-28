184715e5492d4fef515a1432fd560eb24089ce64
MrTradeBuddy <yolkgames@gmail.com>
2025-04-28 16:33:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,42 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
+from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-# Static Files Serve பண்ண
+# Templates & Static mount
+templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Templates
-templates = Jinja2Templates(directory="templates")
-
-@app.get("/", response_class=HTMLResponse)
+# Home page
+@app.get("/")
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+# API endpoint for full company list
+@app.get("/get_companies")
+async def get_companies():
+    companies = [
+        "Reliance Industries Limited",
+        "Tata Consultancy Services (TCS)",
+        "HDFC Bank Limited",
+        "ICICI Bank Limited",
+        "Infosys Limited",
+        "Bharti Airtel Limited",
+        "State Bank of India (SBI)",
+        "Bajaj Finance Limited",
+        "Kotak Mahindra Bank Limited",
+        "HCL Technologies Limited",
+        "Adani Enterprises Limited",
+        "Larsen & Toubro Limited (L&T)",
+        "Wipro Limited",
+        "Titan Company Limited",
+        "Asian Paints Limited",
+        "Axis Bank Limited",
+        "Sun Pharmaceutical Industries Limited",
+        "Tata Steel Limited",
+        "Maruti Suzuki India Limited",
+        "UltraTech Cement Limited"
+    ]
+    return JSONResponse(content={"companies": companies})
