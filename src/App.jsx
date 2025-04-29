810b6c1eb18ea3114a4d19abf8e4a44c0727885f
MrTradeBuddy <yolkgames@gmail.com>
2025-04-29 18:47:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import HomePage from "./components/HomePage";
+import FavouriteStocks from "./components/FavouriteStocks";  // ✅ Add this
 
 function App() {
   return (
-    <div>
-      <HomePage />
+    <div className="flex flex-col space-y-6 p-4">
+      <HomePage />           {/* Existing Dashboard / Indices part */}
+      <FavouriteStocks />    {/* New Favourite Stocks Section */}
     </div>
   );
 }
